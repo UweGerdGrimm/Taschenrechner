3c8b2be8e3b90af9fcd3b09f7622ccb288b43290
UweGerdGrimm <139976747+UweGerdGrimm@users.noreply.github.com>
2023-07-19 15:52:23 +0200

    Projektdateien hinzufügen.

---
+++ Taschenrechner/ConsoleView.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Taschenrechner
+{
+    class ConsoleView
+    {
+        private RechnerModel model;
+
+        public ConsoleView(RechnerModel model)
+        {
+            this.model = model;
+            BenutzerWillBeenden = false;
+        }
+
+        public bool BenutzerWillBeenden { get; private set; }
+
+        public void HoleEingabenFuerErsteBerechnungVomBenutzer()
+        {
+            model.ErsteZahl = HoleZahlVomBenutzer();
+            model.Operation = HoleOperatorVomBenutzer();
+            model.ZweiteZahl = HoleZahlVomBenutzer();
+        }
+
+        public void HoleEingabenFuerFortlaufendeBerechnung()
+        {
+            string eingabe = HoleNaechsteAktionVomBenutzer();
+
+            if (eingabe == "FERTIG")
+            {
+                BenutzerWillBeenden = true;
+            }
+            else
+            {
+                model.ErsteZahl = model.Resultat;
+                model.ZweiteZahl = Convert.ToDouble(eingabe);
+            }
+        }
+
+        private string HoleNaechsteAktionVomBenutzer()
+        {
+            Console.Write("Bitte gib eine weitere Zahl ein (FERTIG zum Beenden): ");
+            return Console.ReadLine();
+        }
+
+        private double HoleZahlVomBenutzer()
+        {
+            string zahl;
+            Console.Write("Bitte gib eine Zahl für die Berechnung ein: ");
+            zahl = Console.ReadLine();
+
+            return Convert.ToDouble(zahl);
+        }
+
+        private string HoleOperatorVomBenutzer()
+        {
+            Console.Write("Bitte gib die auszuführende Operation ein (+, -, /, *): ");
+            return Console.ReadLine();
+        }
+
+        public void GibResultatAus()
+        {
+            switch (model.Operation)
+            {
+                case "+":
+                    Console.WriteLine("Die Summe ist: {0}", model.Resultat);
+                    break;
+
+                case "-":
+                    Console.WriteLine("Die Differenz ist: {0}", model.Resultat);
+                    break;
+
+                case "/":
+                    Console.WriteLine("Der Wert des Quotienten ist: {0}", model.Resultat);
+                    break;
+
+                case "*":
+                    Console.WriteLine("Das Produkt ist: {0}", model.Resultat);
+                    break;
+
+                default:
+                    Console.WriteLine("Du hast eine ungültige Auswahl der Operation getroffen.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Taschenrechner/RechnerModel.cs
@@ -0,0 +1,63 @@
+﻿namespace Taschenrechner
+{
+    class RechnerModel
+    {
+        public double Resultat { get; private set; }
+        public string Operation { get; set; }
+        public double ErsteZahl { get; set; }
+        public double ZweiteZahl { get; set; }
+
+        public RechnerModel()
+        {
+            Resultat = 0;
+            Operation = "unbekannt";
+        }
+
+        public void Berechne()
+        {
+            switch (Operation)
+            {
+                case "+":
+                    Resultat = Addiere(ErsteZahl, ZweiteZahl);
+                    break;
+
+                case "-":
+                    Resultat = Subtrahiere(ErsteZahl, ZweiteZahl);
+                    break;
+
+                case "/":
+                    Resultat = Dividiere(ErsteZahl, ZweiteZahl);
+                    break;
+
+                case "*":
+                    Resultat = Multipliziere(ErsteZahl, ZweiteZahl);
+                    break;
+            }
+        }
+
+        private double Addiere(double ersterSummand, double zweiterSummand)
+        {
+            double summe = ersterSummand + zweiterSummand;
+
+            return summe;
+        }
+
+        private double Subtrahiere(double minuend, double subtrahent)
+        {
+            double differenz = minuend - subtrahent;
+
+            return differenz;
+        }
+
+        private double Dividiere(double dividend, double divisor)
+        {
+            return dividend / divisor;
+        }
+
+        private double Multipliziere(double multiplikator, double multiplikand)
+        {
+            return multiplikator * multiplikand;
+        }
+
+    }
+}
